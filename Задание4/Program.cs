2ea12462d864ec58643350664bf4f9df7ea77506
slay169 <beztorga8@gmail.com>
2022-12-21 22:52:03 +0300

    Выяснить является ли число чётным

---
+++ Задание4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Int32.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
