0457f5347254ee00f19b81e6e3574f6363664355
slay169 <beztorga8@gmail.com>
2022-12-26 20:41:42 +0300

    Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не используя Math.Pow!

---
+++ Задание11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Степень b: ");
+int b = Int32.Parse(Console.ReadLine());
+
+int c = a;
+
+for (int i = 1; i < b; i = i+1)
+{
+c = c * a;
+}
+Console.WriteLine($"a в степени b равно: {c}");
+
