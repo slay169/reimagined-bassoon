ddad28661b71d83698f3671322451869ee3a8d0f
slay169 <beztorga8@gmail.com>
2023-01-03 13:49:58 +0300

    Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами от -10,0 до 10,0.

---
+++ Задание22/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задайте количество строк:");
+int m = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте количество столбцов:");
+int n = Int32.Parse(Console.ReadLine());
+
+double[,] twoDimArray = new double[m, n];
+
+Random rnd = new Random();
+
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ 
+ Console.WriteLine();}}
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
+
